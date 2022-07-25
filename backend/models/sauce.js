95113fd3e74309918418d5467d928c4b7d14c7b2
NonoGitCode <99494935+NonoGitCode@users.noreply.github.com>
2022-07-25 16:56:35 +0200

    avancement du projet

---
+++ backend/models/sauce.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    mainpeper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Sauce',sauceSchema);
